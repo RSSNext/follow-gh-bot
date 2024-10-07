1a32ef1d5866cd6d13ffe9ab9bb668e0e3effc53
Innei <i@innei.in>
2024-10-07 20:16:59 +0800

    update
    Signed-off-by: Innei <i@innei.in>

--- ai.ts
+++ ai.ts
@@ -1,8 +1,7 @@
 import { OpenAI } from 'openai'
 import 'dotenv/config'
-// OpenAI API 配置
+
 export const openai = new OpenAI({
-  // apiKey: process.env.OPENAI_API_KEY,
   apiKey: process.env.OPENAI_API_KEY,
   baseURL: process.env.ENDPOINT,
 })
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Follow PR AI Reviewer Bot
+
+If you have maintainer access, commenting `/ai-review` under the PR triggers it.
